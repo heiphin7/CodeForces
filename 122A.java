8d3d231e4b165e9936ecba37a4806d2032b7c9f8
heiphin7 <152867030+heiphin7@users.noreply.github.com>
2023-12-04 21:56:30 +0600

    Add files via upload

---
+++ 122A.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Football{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        if(str.contains("1111111") || str.contains("0000000")){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
