d768275ca267ae237dbef80e9089a1f5af45a443
heiphin7 <152867030+heiphin7@users.noreply.github.com>
2023-12-04 21:59:53 +0600

    122A.java

--- 122A.java
+++ 122A.java
@@ -4,6 +4,7 @@ public class Football{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
+        // Тут всё очень просто, ищем 7 единиц или 7 нулей подряд через команду constains
         if(str.contains("1111111") || str.contains("0000000")){
             System.out.println("YES");
         }else{
